10906ddcf162b2ff5853d6cc5293c3576bce5f87
Giovanni Rosa da Silva <g.rosa@dom_qp.com.br>
2018-12-13 17:59:46 -0200

    first commit

---
+++ src/components/Burguer/Burguer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import classes from './Burguer.css';
+import BurguerIngredient from './BurguerIngredient/BurguerIngredient';
+
+const Burguer = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_,i) => {
+                return <BurguerIngredient key={igKey + i} type={igKey} />;
+            });
+        });
+
+    return (
+        <div className={classes.Burguer}>
+            <BurguerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurguerIngredient type="bread-bottom" />
+        </div>
+    );
+};
+
+export default Burguer;
---
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Auxiliary';
+import Burguer from '../../components/Burguer/Burguer'
+
+class BurguerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Burguer ingredients={this.state.ingredients}/>
+                <div>Build Controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurguerBuilder;
